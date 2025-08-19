21b89445fe805ed9eea64a07851f4c8dc8889edd
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c1r1p2.42sp.org.br>
2025-08-19 09:59:55 -0300

    feat: create handler deque - deque create - deque push top - deque push bottom - deque remove top - deque remove bottom

--- double_linked_list.h
+++ double_linked_list.h
@@ -6,7 +6,7 @@
 /*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 09:09:26 by jtertuli          #+#    #+#             */
-/*   Updated: 2025/08/18 10:08:22 by jtertuli         ###   ########.fr       */
+/*   Updated: 2025/08/19 09:59:06 by jtertuli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,11 @@ typedef struct s_deque {
 	t_node	*bottom;
 	int		size;
 }	t_deque;
+
+t_deque *ft_deque_create(void);
+void	ft_deque_push_top(t_deque *deque, int value);
+void	ft_deque_push_bottom(t_deque *deque, int value);
+void	ft_deque_pop_top(t_deque *deque);
+void	ft_deque_pop_bottom(t_deque *deque);
 
 #endif
