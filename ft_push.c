a0e7e4b434cd4d71e369e05e5154a1c49c448496
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c2r1p5.42sp.org.br>
2025-08-20 15:54:48 -0300

    feat: push movements

---
+++ ft_push.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 15:22:41 by jtertuli          #+#    #+#             */
+/*   Updated: 2025/08/20 15:51:47 by jtertuli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	pa(t_deque *list_a, t_deque *list_b)
+{
+	t_node *node;
+
+	node = ft_detache_node_by_index(list_b, 0);
+	if (!node)
+		return ;
+	ft_deque_push_top(list_a, node->value);
+}
+
+void	pb(t_deque *list_a, t_deque *list_b)
+{
+	t_node *node;
+
+	node = ft_detache_node_by_index(list_a, 0);
+	if (!node)
+		return ;
+	ft_deque_push_top(list_b, node->value);
+}
